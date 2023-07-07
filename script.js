162a7f413f69b5dd4c3c1e2fa76c4b87ea3547a9
Rushi Patel <patel.rus19@gmail.com>
2023-07-06 21:21:53 -0700

    Added FAQ Section

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-var icon = document.getElementById("icon");
+// Function to handle form submission
+console.log("Hello World"); 
 
-icon.onclick = function(){
-  document.body.classList.toggle("dark-theme"); 
-  if(document.body.classList.contains("dark-theme")){
-    icon.src = "Images/Sun-Icon.png"; 
-  }
-  else{
-    icon.src = "Images/Sun-Icon.png"; 
-  }
-}
+// function calculateIntrinsicValue(event) {
+//   event.preventDefault();
+
+//   // Get input values from the form
+//   const stockTicker = document.getElementById('stock-input').value;
+//   const eps = parseFloat(document.getElementById('eps-input').value);
+//   const peRatio = parseFloat(document.getElementById('pe-input').value);
+//   const growthRate = parseFloat(document.getElementById('growth-input').value);
+//   const discountRate = parseFloat(document.getElementById('discount-input').value);
+//   const currentStockValue = parseFloat(document.getElementById('stock-input').value);
+
+//   // Perform calculations
+//   const intrinsicValue = (eps * (1 + growthRate / 100) * (1 - Math.pow(1 + discountRate / 100, -5))) / (peRatio / 100);
+//   const marginOfSafety = intrinsicValue - currentStockValue;
+
+//   // Display results
+//   const resultElement = document.getElementById('intrinsic-value-result');
+//   resultElement.innerHTML = `
+//     <p>Intrinsic Value: $${intrinsicValue.toFixed(2)}</p>
+//     <p>Margin of Safety: $${marginOfSafety.toFixed(2)}</p>
+//   `;
+
+//   // Enable the display button
+//   const displayButton = document.querySelector('.display-button');
+//   displayButton.disabled = false;
+// }
+
+// // Event listener for form submission
+// const calculatorForm = document.getElementById('calculator-form');
+// calculatorForm.addEventListener('submit', calculateIntrinsicValue);
+
